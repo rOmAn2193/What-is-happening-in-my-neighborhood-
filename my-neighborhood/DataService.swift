4163f14ee79efab0c86d00a0efd073b2a9c64e81
rOmAn2193 <roman_puzey@aol.com>
2016-01-02 15:01:38 -0700

    first commit

---
+++ my-neighborhood/DataService.swift
@@ -0,0 +1,65 @@
+import Foundation
+import UIKit
+
+class DataService
+{
+    static let instance = DataService()
+
+    let KEY_POSTS = "posts"
+    private var _loadedPosts = [Post]()
+
+    var loadedPosts: [Post]
+    {
+        return _loadedPosts
+    }
+
+    func savePosts()
+    {
+        let postsData = NSKeyedArchiver.archivedDataWithRootObject(_loadedPosts)
+        NSUserDefaults.standardUserDefaults().setObject(postsData, forKey: KEY_POSTS) // taking the array of posts, converting into data, stores it on disk, setting an object and giving it a key
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+
+    func loadPosts()
+    {
+        if let postsData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData
+        {
+            if let postsArray = NSKeyedUnarchiver.unarchiveObjectWithData(postsData) as? [Post]
+            {
+                _loadedPosts = postsArray
+            }
+        }
+
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+    }
+
+    func saveImageAndCreatePath(image: UIImage) -> String
+    {
+        let imgData = UIImagePNGRepresentation(image)
+        let imgPath = "image\(NSDate.timeIntervalSinceReferenceDate()).png"
+        let fullPath = documentsPathForFileName(imgPath)
+        imgData?.writeToFile(fullPath, atomically: true)
+        return imgPath
+    }
+
+    func imageForPath(path: String) -> UIImage?
+    {
+        let fullPath = documentsPathForFileName(path)
+        let image = UIImage(named: fullPath)
+        return image
+    }
+
+    func addPost(post: Post)
+    {
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+    }
+
+    func documentsPathForFileName(name: String) -> String
+    {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let fullPath = paths [0] as NSString
+        return fullPath.stringByAppendingPathComponent(name)
+    }
+}
--- my-neighborhood/ViewController.swift
+++ my-neighborhood/ViewController.swift
@@ -1,25 +1,55 @@
-//
-//  ViewController.swift
-//  my-neighborhood
-//
-//  Created by Roman on 1/2/16.
-//  Copyright © 2016 Roman Puzey. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    @IBOutlet weak var tableView: UITableView!
 
-    override func viewDidLoad() {
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        DataService.instance.loadPosts()
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onPostsLoaded", name: "postsLoaded", object: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
 
+        let post = DataService.instance.loadedPosts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell
+        {
+            cell.configureCell(post)
+            return cell
+        }
+        else
+        {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 87.0
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return DataService.instance.loadedPosts.count
+    }
+
+    func onPostsLoaded(notif: AnyObject)
+    {
+        tableView.reloadData()
+    }
 }
 
